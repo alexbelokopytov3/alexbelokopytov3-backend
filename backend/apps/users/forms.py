a6565d97d6f78584ef6b7ea17c57c5a1db17d692
Alex <salllka78@gmail.com>
2022-08-01 22:00:05 +0300

    Create: registration

--- backend/apps/users/forms.py
+++ backend/apps/users/forms.py
@@ -6,3 +6,25 @@ from django.contrib.auth.forms import UserCreationForm
 class Login(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-styling'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-styling'}))
+
+class Registration(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+        help_texts = {
+            'username': None,
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+    def __init__(self, *args, **kwargs):
+      super(Registration, self).__init__(*args, **kwargs)
+      for field in self.fields:
+         self.fields[field].widget.attrs['class'] = 'form-styling'
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,28 +1,110 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import Login
+from .forms import Login, Registration
 from home.views import HomeView
 
 
 def Authentication(request):
+
     if request.method == 'POST':
-        form = Login(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    # return HttpResponse('Authenticated successfully')
-                    return redirect(HomeView)
+        if 'sign_in' in request.POST:
+            form = Login(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user = authenticate(username=cd['username'], password=cd['password'])
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        # return HttpResponse('Authenticated successfully')
+                        return redirect(HomeView)
+                    else:
+                        return HttpResponse('Disabled account')
                 else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
+                    return HttpResponse('Invalid login')
+        elif 'sign_up' in request.POST:
+            reg_form = Registration(request.POST)
+            if reg_form.is_valid():
+                # Create a new user object but avoid saving it yet
+                new_user = reg_form.save(commit=False)
+                # Set the chosen password
+                new_user.set_password(reg_form.cleaned_data['password'])
+                # Save the User object
+                new_user.save()
+                return redirect(HomeView)
     else:
+        reg_form = Registration()
         form = Login()
-    return render(request, 'authentication/authentication.html', {'form': form})
+    return render(request, 'authentication/authentication.html', {'form': form, 'reg_form': reg_form})
+
+
+# def Authentication(request):
+#     if request.method == 'POST':
+#         reg_form = Registration(request.POST)
+#         if reg_form.is_valid():
+#             # Create a new user object but avoid saving it yet
+#             new_user = user_form.save(commit=False)
+#             # Set the chosen password
+#             new_user.set_password(user_form.cleaned_data['password'])
+#             # Save the User object
+#             new_user.save()
+#             return redirect(HomeView)
+#     else:
+#         reg_form = Registration()
+#     if request.method == 'POST':
+#         form = Login(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'], password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     # return HttpResponse('Authenticated successfully')
+#                     return redirect(HomeView)
+#                 else:
+#                     return HttpResponse('Disabled account')
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = Login()
+#     return render(request, 'authentication/authentication.html', {'form': form, 'reg_form': reg_form})
+
+
+# def Register(request):
+#     if request.method == 'POST':
+#         reg_form = Registration(request.POST)
+#         if reg_form.is_valid():
+#             # Create a new user object but avoid saving it yet
+#             new_user = user_form.save(commit=False)
+#             # Set the chosen password
+#             new_user.set_password(user_form.cleaned_data['password'])
+#             # Save the User object
+#             new_user.save()
+#             return redirect(HomeView)
+#     else:
+#         reg_form = Registration()
+#     return render(request, 'authentication/authentication.html', {'user_form': user_form})
+
+
+# def Authentication(request):
+#     if request.method == 'POST':
+#         form = Login(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'], password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     # return HttpResponse('Authenticated successfully')
+#                     return redirect(HomeView)
+#                 else:
+#                     return HttpResponse('Disabled account')
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = Login()
+#     return render(request, 'authentication/authentication.html', {'form': form})
+
 
 # Create your views here.
 
